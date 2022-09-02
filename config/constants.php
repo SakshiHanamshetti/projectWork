8d09e63f70c50284fb26198c5cfcb7530918fa00
SakshiHanamshetti <93385384+SakshiHanamshetti@users.noreply.github.com>
2022-09-03 01:50:47 +0530

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,24 @@
+<?php 
+    //Start Session
+    session_start();
+
+
+    //Create Constants to Store Non Repeating Values
+    define('SITEURL', 'http://localhost/onlinefood-order/');
+    define('LOCALHOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'onlinefoodorder');
+    
+    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD); //or die(mysqli_error()); 
+    $db_select = mysqli_select_db($conn, DB_NAME); //or die(mysqli_error());
+
+    if(!$conn){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    if(!$db_select){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+
+
+?>
