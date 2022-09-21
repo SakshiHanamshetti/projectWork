e41664b7778e9dbaade94706102b417c913e8246
SakshiHanamshetti <93385384+SakshiHanamshetti@users.noreply.github.com>
2022-09-21 21:54:26 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,53 @@
+<?php include('partials-front/menu.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+    
+
+
+<section class="contact">
+        <div class="container">
+            <h2 class="text-center">Contact</h2>
+
+            <div class="background"></div> 
+    <section class="review" id="review">
+    
+        <div class="box-container">
+    
+            <div class="box">
+                <img src="images/ronit1.jpeg" alt="">
+                <h3>Ronit Bangard</h3>
+                
+                <p>Contact: 8107468143</p>
+                <p>Mail: ronitbangard.rb@gmail.com</p>
+                <p><a href="https://instagram.com/ronitbangard?utm_medium=copy_link" style="color:#fff" target="blank"> Insta Handle</a></p>
+            </div>
+            <div class="box">
+                <img src="images/SakshiH.png" alt="">
+                <h3>Sakshi Hanamshetti</h3>
+                
+                <p>Contact: 9420467788</p>
+                <p>Mail: saakshish8@gmail.com</p>
+                <p><a href="https://instagram.com/sakshi_hanamshetti?utm_medium=copy_link" style="color:#fff" target="blank"> Insta Handle</a></p>
+            </div>
+            <div class="box">
+                <img src="images/sakshi1.jpeg" alt="">
+                <h3>Sakshi Shahi</h3>
+                
+                <p> Contact: 7004774075</p>
+                <p>Mail: sakshi123shahi@gmail.com</p>
+                <p><a href="https://instagram.com/sakshi.shahi_06?utm_medium=copy_link" style="color:#fff" target="blank"> Insta Handle</a></p>
+             </div>
+    
+        </div>
+    </section>
+
+    </body>
+</html>
